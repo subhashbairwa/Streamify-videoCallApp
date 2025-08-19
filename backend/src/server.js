7cb3cf5ed1f6a7e6674a8f59ea66014148734b3f
Subhash <Git: Enable>
2025-08-20 00:35:19 +0530

    My first project commit

---
+++ backend/src/server.js
@@ -0,0 +1,39 @@
+import express from "express";
+import "dotenv/config";
+import authRoutes from "./routes/auth.route.js";
+import userRoutes from "./routes/user.route.js";
+import chatRoutes from "./routes/chat.route.js"
+import {connectDB} from "./lib/db.js"
+import cookieParser from "cookie-parser"
+import cors from "cors";
+import path from "path"
+
+
+const app= express();
+app.use(express.json());
+app.use(cookieParser());
+
+const PORT = process.env.PORT
+const __dirname=path.resolve();
+
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true
+}));
+
+app.use("/api/auth",authRoutes);
+app.use("/api/users",userRoutes);
+app.use("/api/chat",chatRoutes);
+
+if(process.env.NODE_ENV=== "production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")));
+
+    app.get("*", (req,res)=>{
+        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
+    })
+}
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on http://localhost:${PORT}`);
+    connectDB();
+});
